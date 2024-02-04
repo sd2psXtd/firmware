b50a79d95f4d951774f68c7f270fa4d4ff988f3c
BBsan2k <bbsan@gmx.de>
2024-02-04 12:00:36 +0100

    Update Cardman for game id

--- src/ps2/ps2_cardman.h
+++ src/ps2/ps2_cardman.h
@@ -8,6 +8,14 @@
 #define PS2_CARD_SIZE_1M        (1024 * 1024)
 #define PS2_CARD_SIZE_512K      (512 * 1024)
 
+#define PS2_CARD_IDX_SPECIAL 0
+
+typedef enum  {
+    PS2_CM_STATE_BOOT,
+    PS2_CM_STATE_GAMEID,
+    PS2_CM_STATE_NORMAL
+} ps2_cardman_state_t;
+
 void ps2_cardman_init(void);
 int ps2_cardman_write_sector(int sector, void *buf512);
 void ps2_cardman_flush(void);
@@ -17,8 +25,11 @@ int ps2_cardman_get_idx(void);
 int ps2_cardman_get_channel(void);
 uint32_t ps2_cardman_get_card_size(void);
 
+void ps2_cardman_set_channel(uint16_t num);
 void ps2_cardman_next_channel(void);
 void ps2_cardman_prev_channel(void);
+
+void ps2_cardman_set_idx(uint16_t num);
 void ps2_cardman_next_idx(void);
 void ps2_cardman_prev_idx(void);
 
@@ -26,3 +37,9 @@ typedef void (*cardman_cb_t)(int);
 
 void ps2_cardman_set_progress_cb(cardman_cb_t func);
 char *ps2_cardman_get_progress_text(void);
+
+void ps2_cardman_set_gameid(const char* game_id);
+const char* ps2_cardman_get_gameid(void);
+const char* ps2_cardman_get_gamename(void);
+const char* ps2_cardman_get_folder_name(void);
+ps2_cardman_state_t ps2_cardman_get_state(void);
