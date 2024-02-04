73fd26afffabaedfadbfab63038baa37e9a49b98
BBsan2k <bbsan@gmx.de>
2024-02-04 11:50:27 +0100

    Add mcfat

---
+++ ext/mcfat/src/mcfat_internal.h
@@ -0,0 +1,11 @@
+#ifndef _MCFAT_INTERNAL_H
+#define _MCFAT_INTERNAL_H
+
+#include "mcfat.h"
+
+
+extern bool mcfat_cardchanged;
+extern mcfat_cardspecs_t mcfat_cardspecs;
+extern mcfat_mcops_t mcfat_bdoperations;
+
+#endif
