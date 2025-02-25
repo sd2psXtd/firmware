bb57c24b833c94de414d723a6a1655b23a487483
BBsan <bbsan2k@gmail.com>
2025-02-25 08:05:56 +0100

    Fix Log Level

--- src/debug.h
+++ src/debug.h
@@ -16,7 +16,7 @@
 #define LOG_LEVEL_PS2_S2M    2
 #define LOG_LEVEL_GUI        2
 #define LOG_LEVEL_CARD_CONF  2
-#define LOG_LEVEL_PS1_MC     4
+#define LOG_LEVEL_PS1_MC     2
 
 #define LOG_ERROR 1
 #define LOG_WARN 2
